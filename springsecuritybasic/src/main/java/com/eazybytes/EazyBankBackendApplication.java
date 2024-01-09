82e8949ab463e260bba931180463cec7c9d518d4
Cloudyee <gyonggyong98@gmail.com>
2024-01-09 14:59:05 +0900

    refactor : 폴더 구조 수정

--- springsecuritybasic/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecuritybasic/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springsecuritybasic;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //실행해줄 파일을 parant외부에 설정해주었을 경우
 //@ComonentScan("해당 클래스 경로")로 작성해줄 수 있다. /-> Optional
 @SpringBootApplication
-public class SpringsecuritybasicApplication {
+public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+		SpringApplication.run(EazyBankBackendApplication.class, args);
 	}
 
 }
--- springsecuritybasic/src/main/java/com/eazybytes/controller/WelcomeController.java
+++ springsecuritybasic/src/main/java/com/eazybytes/controller/WelcomeController.java
@@ -1,11 +1,11 @@
-package com.eazybytes.springsecuritybasic.controller;
+package com.eazybytes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String sayWelcome(){
         return "Welcome to Spring Application with Security";
     }
