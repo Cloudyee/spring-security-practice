f77e59ea054c6eabb90c1a4fbf7faad22f50790d
Cloudyee <gyonggyong98@gmail.com>
2024-01-09 16:38:51 +0900

    chore : 버전 변경

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,16 +3,20 @@ package com.eazybytes.config;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
     //커스텀 Bean 제작 완료
+    //spring security 7부터 람다식으로 문법이 변경된다. 참고!
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+        http.authorizeRequests()
+                .requestMatchers("/mycards", "/myAccount", "/myLoans", "/myBalance").authenticated()
+                .requestMatchers("/contact", "/notices").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
         return http.build();
     }
 }
