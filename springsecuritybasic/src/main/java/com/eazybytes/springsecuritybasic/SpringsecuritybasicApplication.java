837cd390bd43bd5dd1f174cd8cecbe0421f013e2
Cloudyee <gyonggyong98@gmail.com>
2024-01-09 10:43:38 +0900

    add : 기본 controller 추가

--- springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
+++ springsecuritybasic/src/main/java/com/eazybytes/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -3,6 +3,8 @@ package com.eazybytes.springsecuritybasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//실행해줄 파일을 parant외부에 설정해주었을 경우
+//@ComonentScan("해당 클래스 경로")로 작성해줄 수 있다. /-> Optional
 @SpringBootApplication
 public class SpringsecuritybasicApplication {
 
